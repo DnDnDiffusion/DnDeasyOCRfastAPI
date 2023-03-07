bc34c0fb5807415eac062a688590df325397f1a2
somya-15 <somyatomar15@gmail.com>
2023-03-07 19:34:24 +0530

    functions to filter more info from character sheet

--- main.py
+++ main.py
@@ -28,15 +28,55 @@ def class_level(ocrtext):
         return "Not a Class"
 # look through the text and return what colour the dragonborn is
 def dragonborn_colour(ocrtext):
-    classes = ["(Black Dragon Ancestry)", "(Blue Dragon Ancestry)", "(Brass Dragon Ancestry)", "(Bronze Dragon Ancestry)", "(Copper Dragon Ancestry)", "(Gold Dragon Ancestry)", "(Green Dragon Ancestry)", "(Red Dragon Ancestry)", "(Silver Dragon Ancestry)", "(White Dragon Ancestry)"]
-    pattern = '|'.join(classes)
+    colours = ["(Black Dragon Ancestry)", "(Blue Dragon Ancestry)", "(Brass Dragon Ancestry)", "(Bronze Dragon Ancestry)", "(Copper Dragon Ancestry)", "(Gold Dragon Ancestry)", "(Green Dragon Ancestry)", "(Red Dragon Ancestry)", "(Silver Dragon Ancestry)", "(White Dragon Ancestry)"]
+    pattern = '|'.join(colours)
     match = re.search(pattern, ocrtext, re.IGNORECASE)
     if match:
         return match.group()
     else:
         return "Not a Dragonborn"
+    
+# look through the text and return what the background is
+def background(ocrtext):
+    backgrounds = ["Acolyte", "Charlatan", "Criminal", "Entertainer", "Folk Hero", "Guild Artisan", "Hermit", "Noble", "Outlander", "Sage", "Sailor", "Soldier", "Urchin", "Anthropologist", "Archaeologist", "Black Fist Double Agent", "City Watch", "Clan Crafter", "Cloistered Scholar", "Courtier", "Criminal Spy", "Dissenter", "Dragon Casualty", "Earthspur Miner", "Faction Agent", "Far Traveler", "Firefighter", "Fist of Hextor", "Gate Urchin", "Gladiator", "Harper", "Haunted One", "House Agent", "Inheritor", "Initiate", "Inquisitor", "Iron Route Bandit", "Knight of the Order", "Mercenary Veteran", "Mulmaster Aristocrat", "Noble Duelist", "Order of the Gauntlet", "Outcast", "Pirate"]
+    pattern = '|'.join(backgrounds)
+    match = re.search(pattern, ocrtext, re.IGNORECASE)
+    if match:
+        return match.group()
+    else:
+        return "Not a Background"
 
+# look through the text and return what Armor is Worn
+def armor_worn(ocrtext):
+    pattern = r"Armor:\s*(\w+)"
+    # Search for the pattern in the string
+    match = re.search(pattern, ocrtext)
+    # Extract the matched substring
+    if match:
+        substring = match.group(1)
+        if substring == "none":
+            return "clothes"
+        return substring
+    else:
+        return "No Armor"
 
+def weapon(ocrtext):
+    weapons = ["Club", "Dagger", "Greatclub", "Handaxe", "Javelin", "Light Hammer", "Mace", "Quarterstaff","Sickle", "Spear", "Crossbow, light", "Dart", "Shortbow", "Sling", "Battleaxe", "Flail", "Glaive", "Greataxe", "Greatsword", "Halberd", "Lance", "Longsword", "Maul", "Morningstar","Pike", "Rapier", "Scimitar", "Shortsword", "Trident", "War pick", "Warhammer", "Whip", "Blowgun", "Crossbow, hand", "Crossbow, heavy", "Longbow", "Net"]
+    pattern = '|'.join(weapons)
+    match = re.search(pattern, ocrtext, re.IGNORECASE)
+    if match:
+        return match.group()
+    else:
+        return "No weapon"
+    
+def mood_and_tone(ocrtext):
+    alignments = ['Lawful Good', 'Neutral Good', 'Chaotic Good', 'Lawful Neutral', 'True Neutral', 'Chaotic Neutral', 'Lawful Evil', 'Neutral Evil', 'Chaotic Evil']
+    pattern = '|'.join(alignments)
+    match = re.search(pattern, ocrtext, re.IGNORECASE)
+    if match:
+        return match.group()
+    else:
+        return "No alignment"
 
 @app.get("/")
 async def root():
@@ -54,23 +94,11 @@ async def do_ocr(request: Request, file: UploadFile = File(...)):
         res = ocr.readtext(imgFile)
         ocrtext = str(res)
         # return array of strings
-        return [dragonborn_colour(ocrtext),class_level(ocrtext)]
+        return [ocrtext, dragonborn_colour(ocrtext),class_level(ocrtext),background(ocrtext),armor_worn(ocrtext), weapon(ocrtext), mood_and_tone(ocrtext)]
         # probable_text = "\n".join((item[1] for item in res))
         # return StreamingResponse(
         #     io.BytesIO(probable_text.encode()), media_type="text/plain"
         # )
-
-    return {"error": "missing file"}
-
-
-@app.post("/ocr_form")
-async def do_ocr_form(request: Request, file: UploadFile = File(...)):
-    # form = await request.form()
-    # file = form.get("file", None)
-    if file is not None:
-        # res = ocr.readtext(await file.read())
-        res = ocr.readtext(file.file.read())
-        return [item[1] for item in res]
 
     return {"error": "missing file"}
 
